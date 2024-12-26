bef33968d36886e5a07cfe61ad3e7b7944f06ec8
Jcabarus/WSL/DSU <jeromecabarus360@gmail.com>
2024-12-26 17:17:11 -0600

    12/26/2024

--- challenges/coding-challenge-charger-uptime/env/charger-uptime-solution.c
+++ challenges/coding-challenge-charger-uptime/env/charger-uptime-solution.c
@@ -22,39 +22,67 @@
             `stdout` as follows, and then exit gracefully.
 
     Issue:
-        [] Find a way to read in to the file without switching into a binary format
+        [/] Find a way to read in to the file without switching into a binary format
         [] Review how file stream works
-
+        [] Find a way to only read up to the end of file
+        [] Find a way to combine one individual digit into one digit ([1] [1] => [11])
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-void Input(FILE* file);
+void Input(FILE* filestream);
 void Calculation();
 void Output();
 
 int main(int argc, char* argv[])
 {
-    if(argc != 2)
+    if(argc != 2) // CML error handling
     {
         if(argc < 2)
         {
            printf("Error: too few argument (expected 2)\n");
-           return 0;
+           return -1;
         }
         else if(argc > 2)
         {
            printf("Error: too many argument (expected 2)\n");
-           return 0;
+           return -1;
         }
     }
     else
     {
-        FILE* finput = fopen(argv[1], "r");
+        FILE* rstream = fopen(argv[1], "r");
 
-        Input(finput);
+        if(rstream == NULL) // File stream Error handing
+        {
+            printf("Error: unable to open file\n");
+            return -1;
+        }
+        else
+        {
+            Input(rstream);
+            return 0;
+        }
 
         return 0;
     }
+}
+
+void Input(FILE* filestream)
+{
+    int size = 197;
+    char arr[size];
+
+    for(int i = 0; i < size; i++)
+    {
+        fscanf(filestream, "%c", &arr[i]);
+    }
+
+    for(int i = 0; i < size; i++)
+    {
+        printf("%c", arr[i]);
+    }
+
+    printf("\n");
 }
