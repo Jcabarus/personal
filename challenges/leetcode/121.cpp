3ae6df31a7a6b4279e8e9bd14c079e76c8b5f2fc
Jcabarus/WSL <jeromecabarus360@gmail.com>
2025-03-19 11:27:43 -0500

    added files 03/19/2024

--- challenges/leetcode/121.cpp
+++ challenges/leetcode/121.cpp
@@ -13,3 +13,69 @@ class Solution
             
         }
 };
+
+// int maxProfit(vector<int>& prices) 
+// {
+//     int buy_index = 0, sell_index = 0, market_value = 0;
+
+//     if(prices.size() > 1)
+//     {
+//         while(market_value == 0)
+//         {
+//             for(int i = 1 + market_value; i < prices.size(); i++) // Finds the maximum value for sell_index
+//             {
+//                 if(prices[market_value] <= prices[i])
+//                 {
+//                     market_value = i;
+//                 }
+//                 else if(market_value == 0)
+//                 {
+//                     market_value++;
+//                 }
+//             }
+
+//             sell_index = market_value;
+//         }
+//     }
+
+//     market_value = 0;
+
+//     for(int i = 1; i < prices.size() - (prices.size() - (sell_index + 1)); i++) // Finds the minimum value for buy_index
+//     {
+//         if(prices[market_value] > prices[i])
+//         {
+//             market_value = i;
+//         }
+//     }
+
+//     buy_index = market_value;
+
+//     return prices[sell_index] - prices[buy_index];
+// }
+    
+// int maxProfit(vector<int>& prices) 
+// {
+//     int buy_index = 0, sell_index = 0, market_value = 0;
+
+//     for(int i = 1; i < prices.size() - (); i++) // Finds the minimum value for buy_index
+//     {
+//         if(prices[market_value] > prices[i])
+//         {
+//             market_value = i;
+//         }
+//     }
+
+//     buy_index = market_value;
+
+//     for(int i = market_value; i < prices.size(); i++) // Finds the maximum value for sell_index
+//     {
+//         if(prices[market_value] < prices[i])
+//         {
+//             market_value = i;
+//         }
+//     }
+
+//     sell_index = market_value;
+    
+//     return prices[sell_index] - prices[buy_index];
+// }
--- challenges/leetcode/test.cpp
+++ challenges/leetcode/test.cpp
@@ -1,4 +1,4 @@
-#include "189.cpp"
+#include "121.cpp"
 
 void Test(int test_case);
 void Run(int test[], int size);
@@ -20,20 +20,26 @@ int main(int argc, char* argv[])
 
 void Test(int test_case)
 {
-    int test1[] = {1, 2, 3, 4, 5};
-    int test2[] = {2, 2, 1, 1, 1, 2, 2};
-    int test3[] = {4, 4, 3, 7, 6, 5, 4, 8, 2, 6, 7, 8, 5, 8, 3, 7, 9, 1, 0, 3, 6};
-    int test4[] = {1, 0};
-    int test5[] = {0, 0, 0};
+    int test1[] = {1, 3, 4, 5, 2, 9, 8}; // 1, 9, 8
+    int test2[] = {2, 4, 5, 1, 6, 8, 9}; // 1, 9, 8
+    int test3[] = {7, 2, 5, 3, 1, 4}; // 1, 4, 3
+    int test4[] = {1, 2}; // 1, 1, 0
+    int test5[] = {3, 2, 6, 5, 0, 3}; // 2, 6, 4
+    int test6[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 1, 1, 1, 1, 1, 34}; // 1, 34, 33
+    int test7[] = {34, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}; // 34, 34, 0
+    int test8[] = {2, 4, 1}; // 2, 4, 2
     
     switch(test_case)
     {
-        case(1): Run(test1, 5); break;
+        case(1): Run(test1, 7); break;
         case(2): Run(test2, 7); break;
-        case(3): Run(test3, 21); break;
+        case(3): Run(test3, 6); break;
         case(4): Run(test4, 2); break;
-        case(5): Run(test5, 3); break;
-        default: return;
+        case(5): Run(test5, 6); break;
+        case(6): Run(test6, 21); break;
+        case(7): Run(test7, 21); break;
+        case(8): Run(test8, 3); break;
+        default: cout << "Invalid test case" << endl; return;
     }
 }
 
@@ -42,23 +48,11 @@ void Run(int test[], int size)
     Solution solution;
 
     vector<int> test_array;
-    int num_rotate;
 
     for(int i = 0; i < size; i++)
     {
         test_array.push_back(test[i]);
     }
-
-    cin >> num_rotate;
     
-    solution.rotate(test_array, num_rotate);
-    
-    for(int i = 0; i < test_array.size(); i++)
-    {
-        cout << test_array[i] << " ";
-    }
-
-    cout << endl;
-
-
+    cout << solution.maxProfit(test_array) << endl;
 }
