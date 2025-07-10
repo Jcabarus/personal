2f145ce8c95e9f68eb78422d1a3d448a41ede751
Jcabarus/WSL <jeromecabarus360@gmail.com>
2025-07-10 01:45:30 -0500

    [*][bug fix in progress in pratt_parsing.cpp:26]

--- projects/concept-ideas/calculator/src/pratt_parsing.cpp
+++ projects/concept-ideas/calculator/src/pratt_parsing.cpp
@@ -3,8 +3,8 @@
 Pratt::Pratt(vector<Token*> &tokenized_expression_vector)
 {
     PARSE(tokenized_expression_vector);
-    OPERATION();
-    Result();
+    // OPERATION();
+    // Result   ();
 }
 
 Pratt::~Pratt()
@@ -25,7 +25,8 @@ void Pratt::Result()
 
 void Pratt::PARSE(vector<Token*> &tokenized_expression_vector) // Assigns affinity based on the token, and forms a tree based on its affinity
 {
-    vector<int> operator_index, operand_index;
+    int priority_precedence_index;
+    vector<int> operand_index_position, operator_index_position, operation_index_position;
 
     for(int i = 0; i < tokenized_expression_vector.size(); i++) // Affinity assignment
     {
@@ -34,7 +35,6 @@ void Pratt::PARSE(vector<Token*> &tokenized_expression_vector) // Assigns affini
             PNode* insert_node = INITIALIZE(tokenized_expression_vector[i]);
 
             binary_parse_tree.push_back(insert_node);
-            operand_index.push_back(i);
         }
         else if(tokenized_expression_vector[i]->token_attribute == "operator") // If it's an operator, for simplicity the affinities are in tens plaace
         {
@@ -64,130 +64,84 @@ void Pratt::PARSE(vector<Token*> &tokenized_expression_vector) // Assigns affini
             }
             
             binary_parse_tree.push_back(insert_node);
-            operator_index.push_back(i);
         }
     }
 
-    while(!operator_index.empty())
+    for(int i = 0; i < binary_parse_tree.size(); i++)
     {
-        for(int i = 0; i < operator_index.size(); i++) // Tree formation
+        if(binary_parse_tree[i]->token->token_attribute == "operator")
         {
-            if(i == 0) // Beginning position affinity
-            {
-                binary_parse_tree[operator_index[i]]->pnode_left_link = binary_parse_tree[operand_index[i]];
-                binary_parse_tree[operand_index[i]]->token->token_attribute = "N/A";
-    
-                if(binary_parse_tree[operator_index[i]]->precedence_right > binary_parse_tree[operator_index[i + 1]]->precedence_left) // Is left has a greater affinity?
-                {
-                    binary_parse_tree[operator_index[i]]->pnode_right_link = binary_parse_tree[operand_index[i + 1]];
-                    binary_parse_tree[operand_index[i + 1]]->token->token_attribute = "N/A";
-                }
-                else // Right as a greater affinity
-                {
-                    binary_parse_tree[operator_index[i + 1]]->pnode_left_link = binary_parse_tree[operand_index[i + 1]];
-                    binary_parse_tree[operand_index[i + 1]]->token->token_attribute = "N/A";
-                }
-            }
-            else if(i < operator_index.size() - 1)// In between affinity
-            {
-                if(binary_parse_tree[operator_index[i]]->precedence_right > binary_parse_tree[operator_index[i + 1]]->precedence_left) // Is left has a greater affinity?
-                {
-                    binary_parse_tree[operator_index[i]]->pnode_right_link = binary_parse_tree[operand_index[i + 1]];
-                }
-                else // Right as a greater affinity
-                {
-                    binary_parse_tree[operator_index[i + 1]]->pnode_left_link = binary_parse_tree[operand_index[i + 1]];
-                }
-    
-                binary_parse_tree[operand_index[i + 1]]->token->token_attribute = "N/A";
-            }
-            else if(i == operator_index.size() - 1) // Last position affinity
-            {
-                binary_parse_tree[operator_index[i]]->pnode_right_link = binary_parse_tree[operand_index[i + 1]];
-                binary_parse_tree[operand_index[i + 1]]->token->token_attribute = "N/A";
-            }
+            operator_index_position.push_back(i);
         }
-
-        for(int i = 0; i < operator_index.size(); i++) // If an operator is a binary tree, it becomes an operand and loses its affinity
+        else if(binary_parse_tree[i]->token->token_attribute == "operand")
         {
-            if(binary_parse_tree[operator_index[i]]->pnode_left_link != nullptr && binary_parse_tree[operator_index[i]]->pnode_right_link != nullptr)
-            {
-                binary_parse_tree[operator_index[i]]->token->token_attribute = "operand";
-                binary_parse_tree[operator_index[i]]->precedence_left = 0;
-                binary_parse_tree[operator_index[i]]->precedence_right = 0;
-            }            
+            operand_index_position.push_back(i);
         }
-
-        cout << " operand_index: ";
-        for(int i = 0; i < operand_index.size(); i++)
-        {
-            cout << binary_parse_tree[operand_index[i]]->token->token_identificator << " ";
-        }
-        cout << endl;
-        
-        cout << "operator_index: ";
-        for(int i = 0; i < operator_index.size(); i++)
-        {
-            cout << binary_parse_tree[operator_index[i]]->token->token_identificator << " ";
-        }
-        cout << endl << endl;
-        
-        operator_index.clear(); // Reset after each iteration 
-        operand_index.clear(); // Reset after each iteration
-        
-        for(int i = 0; i < binary_parse_tree.size(); i++) // Operator index position identifier
-        {
-            if(binary_parse_tree[i]->token->token_attribute == "operator")
-            {
-                operator_index.push_back(i);
-            }
-            else if(binary_parse_tree[i]->token->token_attribute == "operand")
-            {
-                operand_index.push_back(i);
-            }
-        }
-
-        // For debugging purposes, delete later
-        cout << " operand_index: ";
-        for(int i = 0; i < operand_index.size(); i++)
-        {
-            cout << binary_parse_tree[operand_index[i]]->token->token_identificator << " ";
-        }
-        cout << endl;
-        
-        cout << "operator_index: ";
-        for(int i = 0; i < operator_index.size(); i++)
-        {
-            cout << binary_parse_tree[operator_index[i]]->token->token_identificator << " ";
-        }
-        cout << endl << endl;
-    
-        cout << "=============================" << endl << endl;
-        for(int j = 1; j < binary_parse_tree.size(); j = j + 2)
-        {
-            cout << "    PNode: " << binary_parse_tree[j] << endl;
-            // cout << "    |   |---token: " << binary_parse_tree[j]->token << endl;
-            cout << "    |   |---token_identificator: " << binary_parse_tree[j]->token->token_identificator << endl;
-            cout << "    |   |---token_attribute: " << binary_parse_tree[j]->token->token_attribute << endl;
-            // cout << "    |---precedence_left: " << binary_parse_tree[j]->precedence_left << endl;
-            // cout << "    |---precedence_right: " << binary_parse_tree[j]->precedence_right << endl;
-            if(binary_parse_tree[j]->pnode_left_link != nullptr)
-            {
-                cout << "    |---pnode_left_link: " << binary_parse_tree[j]->pnode_left_link->token->token_identificator << endl;
-            }
-
-            if(binary_parse_tree[j]->pnode_right_link != nullptr)
-            {
-                cout << "    |---pnode_right_link: " << binary_parse_tree[j]->pnode_right_link->token->token_identificator << endl;
-            }
-            cout << endl;
-        }
-        cout << "=============================" << endl << endl;
-        // For debugging purposes, delete later
     }
 
-    binary_parse_tree_root = binary_parse_tree[operand_index[0]];
-    operand_index.clear();
+    priority_precedence_index = operator_index_position[0];
+
+    for(int i = 0; i < operator_index_position.size(); i++)
+    {
+        if(binary_parse_tree[priority_precedence_index]->precedence_left == binary_parse_tree[operator_index_position[i]]->precedence_left)
+        {
+            operation_index_position.push_back(operator_index_position[i]);
+        }
+        else if(binary_parse_tree[priority_precedence_index]->precedence_left < binary_parse_tree[operator_index_position[i]]->precedence_left)
+        {
+            operation_index_position.clear();
+            operation_index_position.push_back(operator_index_position[i]);
+            priority_precedence_index = operator_index_position[i];
+        }
+    }
+
+    // For debugging purposes, delete later
+    // cout << "  operand_index_position(" << operand_index_position.size() << "): ";
+    // for(int i = 0; i < operand_index_position.size(); i++)
+    // {
+    //     cout << binary_parse_tree[operand_index_position[i]]->token->token_identificator << " ";
+    // }
+    // cout << endl;
+    
+    // cout << " operator_index_position(" << operator_index_position.size() << "): ";
+    // for(int i = 0; i < operator_index_position.size(); i++)
+    // {
+    //     cout << binary_parse_tree[operator_index_position[i]]->token->token_identificator << " ";
+    // }
+    // cout << endl;
+
+    // cout << "operation_index_position(" << operation_index_position.size() << "): ";
+    // for(int i = 0; i < operation_index_position.size(); i++)
+    // {
+    //     cout << binary_parse_tree[operation_index_position[i]]->token->token_identificator << " ";
+    // }
+    // cout << endl;
+    // For debugging purposes, delete later
+
+        // For debugging purposes, delete later
+        // cout << "=============================" << endl << endl;
+        // for(int j = 1; j < binary_parse_tree.size(); j = j + 2)
+        // {
+        //         cout << "    PNode: " << binary_parse_tree[j] << endl;
+        //     // cout << "    |   |---token: " << binary_parse_tree[j]->token << endl;
+        //     cout << "    |   |---token_identificator: " << binary_parse_tree[j]->token->token_identificator << endl;
+        //     cout << "    |   |---token_attribute: " << binary_parse_tree[j]->token->token_attribute << endl;
+        //     // cout << "    |---precedence_left: " << binary_parse_tree[j]->precedence_left << endl;
+        //     // cout << "    |---precedence_right: " << binary_parse_tree[j]->precedence_right << endl;
+        //     if(binary_parse_tree[j]->pnode_left_link != nullptr)
+        //     {
+        //         cout << "    |---pnode_left_link: " << binary_parse_tree[j]->pnode_left_link->token->token_identificator << endl;
+        //     }
+
+        //     if(binary_parse_tree[j]->pnode_right_link != nullptr)
+        //     {
+        //         cout << "    |---pnode_right_link: " << binary_parse_tree[j]->pnode_right_link->token->token_identificator << endl;
+        //     }
+        //     cout << endl;
+        // }
+        // cout << "=============================" << endl << endl;
+        // For debugging purposes, delete later
+    // }
 }
 
 void Pratt::OPERATION()
