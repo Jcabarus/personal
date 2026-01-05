1bdbc661c8e443bf21b20cd9cfcfd9441f291da2
Jcabarus/MacOS <jeromecabarus360@icloud.com>
2026-01-05 01:18:12 -0600

    [*][added progress]

--- challenges/coding/leetcode/55.cpp
+++ challenges/coding/leetcode/55.cpp
@@ -7,36 +7,76 @@ using namespace std;
 
 bool canJump(vector<int>& nums) 
 {
-    int initial_index = 0;
-    int optimum_jump = nums[initial_index];
+    // {3, 0, 8, 2, 0, 0, 1}
+    // {3, 2, 1, 0, 4}
+    //  {3, 0, 8, 2, 0, 0, 1, 4, 3, 0, 2, 1, 0, 3, 0, 0, 0, 2}
 
-    while(initial_index != nums[nums.size() - 1])
+    vector<int*> unjumped_indices;
+    vector<int*> jumped_indices;
+    int* jump_end_goal = nullptr;
+    int* current_evaluate_jump_ptr = nullptr;
+    int* previous_evaluate_jump_ptr = nullptr;
+    int* jump_ahead_ptr = nullptr;
+    int* evaluated_jump_ptr = nullptr;
+
+
+    jump_end_goal = &nums[nums.size() - 1];
+    current_evaluate_jump_ptr = &nums[0];
+    unjumped_indices.push_back(current_evaluate_jump_ptr);
+    jump_ahead_ptr = unjumped_indices[unjumped_indices.size() - 1];
+
+    while(current_evaluate_jump_ptr != jump_end_goal)
+    // for(int i = 0; i < 8; i++)
     {
-        for(int i = initial_index + 1; i <= nums[i] + 1; i++)
+        // cout << " current_evaluate_jump_ptr: " << *current_evaluate_jump_ptr << endl; // Debug, remove later
+        
+        if(*jump_ahead_ptr == 0)
         {
-            if(optimum_jump < nums[i])
-            {
-                optimum_jump = nums[i];
-            }
-            
-            if(nums[i] == 0 && &nums[i] != &nums[nums.size() - 1])
-            {
-                return false;
-            }
-            
-            initial_index = i;
-        }
-    }
+            // cout << "   | --> unjumped_ahead: " << *jump_ahead_ptr << endl; // Debug, remove later
+            unjumped_indices.pop_back();
+            current_evaluate_jump_ptr = &(*unjumped_indices.back());
+            evaluated_jump_ptr = &(*unjumped_indices.back());
+            jump_ahead_ptr = &(*unjumped_indices.back());
 
+
+            // cout << "            jump_ahead_ptr: " << *jump_ahead_ptr << endl; // Debug, remove later
+        }
+        else if(previous_evaluate_jump_ptr != nullptr && jump_ahead_ptr == previous_evaluate_jump_ptr)
+        {
+            // cout << "   | --> unjumped_ahead: " << *jump_ahead_ptr << endl; // Debug, remove later
+            unjumped_indices.pop_back();
+            current_evaluate_jump_ptr = unjumped_indices.back();
+            previous_evaluate_jump_ptr = nullptr;
+
+            jump_ahead_ptr = &(*unjumped_indices.back());
+            // cout << "            jump_ahead_ptr: " << *jump_ahead_ptr << endl; // Debug, remove later
+        }
+        else
+        {
+            for(int i = 0; i < *current_evaluate_jump_ptr && jump_ahead_ptr != jump_end_goal; i++)
+            {
+                unjumped_indices.push_back(++jump_ahead_ptr);        
+                // cout << "   | <-- jumped_ahead: " << *jump_ahead_ptr << endl; // Debug, remove later
+            }
+            
+            previous_evaluate_jump_ptr = &(*current_evaluate_jump_ptr);
+            // cout << "previous_evaluate_jump_ptr: " << *previous_evaluate_jump_ptr << endl; // Debug, remove later
+            
+            current_evaluate_jump_ptr = &(*jump_ahead_ptr);
+            
+            jump_ahead_ptr = &(*unjumped_indices.back());
+            // cout << "            jump_ahead_ptr: " << *jump_ahead_ptr << endl; // Debug, remove later
+        }
+
+        cout << endl;
+    }
+    
     return true;
 }
 
-int main()
+void cases(vector<int> &num)
 {
-    vector<int> nums_1 = {2, 3, 1, 1, 4};
-    vector<int> nums_2 = {2, 1, 6, 1, 0};
-
-    if(canJump(nums_1))
+    if(canJump(num))
     {
         cout << "Can jump!" << endl;
     }
@@ -44,6 +84,16 @@ int main()
     {
         cout << "Not possible" << endl;
     }
+}
+
+int main()
+{
+    vector<int> nums_1 = {2, 3, 1, 1, 4}; // true
+    vector<int> nums_2 = {3, 2, 1, 0, 4}; // false
+    vector<int> nums_3 = {3, 0, 8, 2, 0, 0, 1}; // 
+    vector<int> final = {3, 0, 8, 2, 0, 0, 1, 4, 3, 0, 2, 1, 0, 3, 0, 0, 0, 2}; // 
+
+    cases(nums_3);
 
     return 0;
 }
